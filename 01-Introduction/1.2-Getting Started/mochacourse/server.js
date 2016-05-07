eae600d6682e9a0b8d415bfb568541da7197bde9
ahmed-hamdy90 <ahmedhamdy20@gmail.com>
2016-05-08 00:49:49 +0200

    introduction/Getting started video: finish writing node server file and fix bug into index.pug file

--- 01-Introduction/1.2-Getting Started/mochacourse/server.js
+++ 01-Introduction/1.2-Getting Started/mochacourse/server.js
@@ -1,1 +1,16 @@
+'use strict';
 
+var express = require('express');
+var app     = express();
+
+app
+  // set template engine will used in express app
+  .set('view engine', 'pug')
+  // set express app Routes
+  .get('/', function(req, res) {
+    res.render('index', {
+      palette : ["#cc7790", "#ff5512", "#75d709"]
+    });
+  })
+  // set port will server listen
+  .listen(9000);
